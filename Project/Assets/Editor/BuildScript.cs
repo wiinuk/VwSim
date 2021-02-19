b87caa066f545f9bb0040dc3ecd769eae5e7fd8e
wiinuk <wii.nukegara@gmail.com>
2021-02-19 22:47:37 +0900

    ビルドスクリプトを追加

---
+++ Project/Assets/Editor/BuildScript.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+
+namespace Editor
+{
+    public static class BuildScript
+    {
+        static void CreateFolder(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        static IEnumerable<string> GetAllScenePaths()
+        {
+            foreach (var scene in EditorBuildSettings.scenes)
+            {
+                if (scene.enabled)
+                {
+                    yield return scene.path;
+                }
+            }
+        }
+        static bool isToolBar = false;
+        public static void Build()
+        {
+            try
+            {
+                var outputPath = "../out/WebGL";
+                Console.WriteLine($"{nameof(outputPath)}: {Path.GetFullPath(outputPath)}");
+                CreateFolder(outputPath);
+
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.WebGL, "");
+                var report = BuildPipeline.BuildPlayer(new BuildPlayerOptions
+                {
+                    options = BuildOptions.None,
+                    scenes = GetAllScenePaths().ToArray(),
+                    target = BuildTarget.WebGL,
+                    locationPathName = outputPath,
+                });
+
+                if (isToolBar) { return; }
+
+                EditorApplication.Exit(
+                    report.summary.result == BuildResult.Succeeded ? 0 : 1
+                );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        [MenuItem("ビルド/アプリをビルド")]
+        static void BuildApp() {
+            isToolBar = true;
+            Build();
+        }
+    }
+}
